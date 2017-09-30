88c69049640be9ef65ce5dca6bb651712ea8ab70
Jacob Hardin-Bernhardt <jAKEHB2@gmail.com>
2017-09-29 21:10:55 -0400

    Removing File
    Removed file

--- jQueryI.js
+++ jQueryI.js
@@ -1,1 +1,7 @@
-alert("Hello World!");
+$("#Submit").click(function () {
+	alert("Your data is being processed... please wait while we file your information.");
+	const politicalAllegiance = $("#politicalAllegiance").val();
+	const citizenship = $("#citizenship").val();
+	console.log(politicalAllegiance, citizenship);
+	$("p").append(politicalAllegiance, citizenship)
+});
